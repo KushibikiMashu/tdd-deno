688d407fc33173df6cd28b8805c610aa41de679f
KushibikiMashu <kushibiki3413@gmail.com>
2022-07-23 21:26:36 +0900

    feat: Chapter 11: The Root of All Evil

--- src/money.test.ts
+++ src/money.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals, assert, assertFalse } from "https://deno.land/std@0.149.0/testing/asserts.ts";
-import {Franc, Money} from "./money.ts";
+import { Money } from "./money.ts";
 
 // TODO List
 // [ ] $5 + 10CHF = $10 (when rating is 2:1)
@@ -11,45 +11,26 @@ import {Franc, Money} from "./money.ts";
 // [ ] hashCode()
 // [ ] To evaluate equality to null
 // [ ] To evaluate equality to other objects
+// [x] Duplication of Dollar and Franc
 // [x] To generalize equals method
-// [ ] To generalize times method
+// [x] To generalize times method
 // [x] To compare Franc and Dollar
 // [x] Concept of currency
-// [ ] To delete testFrancMultiplication ?
+// [x] should delete testFrancMultiplication ?
 
 Deno.test('multiplication',   () => {
-  // Comment out here because comparing parent class and sub class here.
-  //  -   Dollar {
-  //  +   Money {
-
-  // const five = Money.dollar(5);
-  // assertEquals(Money.dollar(10), five.times(2))
-  // assertEquals(Money.dollar(15), five.times(3))
+  const five = Money.dollar(5);
+  assertEquals(Money.dollar(10), five.times(2))
+  assertEquals(Money.dollar(15), five.times(3))
 })
 
 Deno.test('equality', () => {
   assert(Money.dollar(5).equals(Money.dollar(5)))
   assertFalse(Money.dollar(5).equals(Money.dollar(6)))
-  assert(Money.franc(5).equals(Money.franc(5)))
-  assertFalse(Money.franc(5).equals(Money.franc(6)))
   assertFalse(Money.franc(5).equals(Money.dollar(5)))
-})
-
-Deno.test('franc multiplication',   () => {
-  // Comment out here because comparing parent class and sub class here.
-  //  -   Franc {
-  //  +   Money {
-
-  // const five = Money.franc(5);
-  // assertEquals(Money.franc(10), five.times(2))
-  // assertEquals(Money.franc(15), five.times(3))
 })
 
 Deno.test('currency',   () => {
   assertEquals('USD', Money.dollar(1).getCurrency())
   assertEquals('CHF', Money.franc(1).getCurrency())
-})
-
-Deno.test('different class equality',   () => {
-  assert(new Money(10, 'CHF').equals(new Franc(10, 'CHF')))
 })
--- src/money.ts
+++ src/money.ts
@@ -17,22 +17,10 @@ export class Money {
   }
 
   static dollar(amount: number): Money {
-    return new Dollar(amount,  'USD')
+    return new Money(amount,  'USD')
   }
 
   static franc(amount: number): Money {
-    return new Franc(amount, 'CHF')
-  }
-}
-
-export class Dollar extends Money {
-  constructor(protected amount: number, protected currency: string) {
-    super(amount, currency)
-  }
-}
-
-export class Franc extends Money {
-  constructor(protected amount: number, protected currency: string) {
-    super(amount, currency)
+    return new Money(amount, 'CHF')
   }
 }
