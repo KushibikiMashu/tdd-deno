bf5408b61e94137a1ab1fe61ca9e6d4e3ae06e8c
KushibikiMashu <kushibiki3413@gmail.com>
2022-07-23 20:46:51 +0900

    feat: Chapter 8: Making Objects

--- src/money.test.ts
+++ src/money.test.ts
@@ -1,6 +1,5 @@
 import { assertEquals, assert, assertFalse } from "https://deno.land/std@0.149.0/testing/asserts.ts";
-import {Dollar} from "./dollar.ts";
-import {Franc} from "./franc.ts";
+import { Money } from "./money.ts";
 
 // TODO List
 // [ ] $5 + 10CHF = $10 (when rating is 2:1)
@@ -14,24 +13,25 @@ import {Franc} from "./franc.ts";
 // [ ] To evaluate equality to other objects
 // [x] To generalize equals method
 // [ ] To generalize times method
-// [ ] To compare Franc and Dollar
+// [x] To compare Franc and Dollar
+// [ ] Concept of currency
 
 Deno.test('multiplication',   () => {
-  const five = new Dollar(5);
-  assertEquals(new Dollar(10), five.times(2))
-  assertEquals(new Dollar(15), five.times(3))
+  const five = Money.dollar(5);
+  assertEquals(Money.dollar(10), five.times(2))
+  assertEquals(Money.dollar(15), five.times(3))
 })
 
 Deno.test('equality', () => {
-  assert(new Dollar(5).equals(new Dollar(5)))
-  assertFalse(new Dollar(5).equals(new Dollar(6)))
-  assert(new Franc(5).equals(new Franc(5)))
-  assertFalse(new Franc(5).equals(new Franc(6)))
-  assertFalse(new Franc(5).equals(new Dollar(5)))
+  assert(Money.dollar(5).equals(Money.dollar(5)))
+  assertFalse(Money.dollar(5).equals(Money.dollar(6)))
+  assert(Money.franc(5).equals(Money.franc(5)))
+  assertFalse(Money.franc(5).equals(Money.franc(6)))
+  assertFalse(Money.franc(5).equals(Money.dollar(5)))
 })
 
 Deno.test('franc multiplication',   () => {
-  const five = new Franc(5);
-  assertEquals(new Franc(10), five.times(2))
-  assertEquals(new Franc(15), five.times(3))
+  const five = Money.franc(5);
+  assertEquals(Money.franc(10), five.times(2))
+  assertEquals(Money.franc(15), five.times(3))
 })
--- src/money.ts
+++ src/money.ts
@@ -1,10 +1,40 @@
-export class Money {
+export abstract class Money {
   constructor(protected amount: number) {
   }
+
+  abstract times(multiplier: number): Money;
 
   equals(object: Money): boolean {
     return this.amount === (object as Money).amount
       // "object instanceof this" doesn't work so comparing both class names here.
       && object.constructor.name === this.constructor.name;
+  }
+
+  static dollar(amount: number): Money {
+    return new Dollar(amount)
+  }
+
+  static franc(amount: number): Money {
+    return new Franc(amount)
+  }
+}
+
+export class Dollar extends Money {
+  constructor(protected amount: number) {
+    super(amount)
+  }
+
+  times(multiplier: number): Money {
+    return new Dollar(this.amount * multiplier)
+  }
+}
+
+export class Franc extends Money {
+  constructor(protected amount: number) {
+    super(amount)
+  }
+
+  times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier)
   }
 }
