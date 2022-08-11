7218c5f659eb81d9920fa5500ea2998254262bef
KushibikiMashu <kushibiki3413@gmail.com>
2022-08-12 01:20:35 +0900

    chapter 21

--- xunit/xunit.ts
+++ xunit/xunit.ts
@@ -6,10 +6,10 @@ import { assert } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 // [x] run tearDown after
 // [ ] run tearDown even if a test failed
 // [ ] run multiple tests
-// [ ] output collected test results
+// [x] output collected test results
 // [x] log string at WasRun
 
-type Method = "run" | "testMethod" | "testRunning";
+type Method = "run" | "testTemplateMethod" | "testResult" | "testFailedResult";
 
 class TestCase {
   name: Method;
@@ -22,10 +22,14 @@ class TestCase {
   }
 
   run = () => {
+    const result = new TestResult()
+    result.testStarted()
     this.setUp();
     const method = (this as any)[this.name];
     method();
     this.tearDown();
+
+    return result
   };
 
   tearDown() {
@@ -43,17 +47,48 @@ class WasRun extends TestCase {
     this.log = this.log + "testMethod ";
   };
 
+  testBrokenMethod = () => {
+    throw new Error()
+  }
+
   tearDown() {
     this.log = this.log + "tearDown ";
   }
 }
 
+class TestResult {
+  constructor(private runCount: number = 0) {
+  }
+
+  testStarted() {
+    this.runCount++
+  }
+
+  summary() {
+    return `${this.runCount} run, 0 failed`
+  }
+}
+
 class TestCaseTest extends TestCase {
-  testSetUp = () => {
+  testTemplateMethod = () => {
     const test = new WasRun("testMethod");
     test.run();
     assert("setUp testMethod tearDown " === test.log);
   };
+
+  testResult = () => {
+    const test = new WasRun("testMethod");
+    const result = test.run();
+    assert("1 run, 0 failed" === result.summary());
+  };
+
+  testFailedResult = () => {
+    const test = new WasRun("testBrokenMethod");
+    const result = test.run();
+    assert("1 run, 1 failed" === result.summary());
+  };
 }
 
-new TestCaseTest("testSetUp").run();
+new TestCaseTest("testTemplateMethod").run();
+new TestCaseTest("testResult").run();
+// new TestCaseTest("testFailedResult").run();
